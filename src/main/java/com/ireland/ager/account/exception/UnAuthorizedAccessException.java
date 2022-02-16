03e0a01d42cd5aa46fd763fbe1adc501581ca718
dhkstnaos <dhkstnaos@gmail.com>
2022-02-16 18:36:40 +0900

    Add account remark

--- src/main/java/com/ireland/ager/account/exception/UnAuthorizedAccessException.java
+++ src/main/java/com/ireland/ager/account/exception/UnAuthorizedAccessException.java
@@ -1,5 +1,9 @@
 package com.ireland.ager.account.exception;
 
+/**
+ * @Class : UnAuthorizedAccessException
+ * @Description : 접근 권한 에러 처리
+ **/
 public class UnAuthorizedAccessException extends RuntimeException {
     public UnAuthorizedAccessException() {
         super();
--- src/main/java/com/ireland/ager/account/service/AccountInfoServiceImpl.java
+++ src/main/java/com/ireland/ager/account/service/AccountInfoServiceImpl.java
@@ -16,6 +16,10 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+/**
+ * @Class : AccountInfoServiceImpl
+ * @Description : 계정 도메인에 대한 서비스
+ **/
 @Service
 @Slf4j
 @RequiredArgsConstructor
@@ -27,22 +31,46 @@ public class AccountInfoServiceImpl {
     private final BoardRepository boardRepository;
     private final TradeRepository tradeRepository;
 
+    /**
+     * @Method : findSellsByAccountId
+     * @Description : 판매 내역 조회
+     * @Parameter : [accountId, pageable]
+     * @Return : Slice<ProductThumbResponse>
+     **/
     public Slice<ProductThumbResponse> findSellsByAccountId(Long accountId, Pageable pageable) {
         return productRepository.findSellProductsByAccountId(accountId, pageable);
     }
 
+    /**
+     * @Method : findBuysByACcountId
+     * @Description : 구매 내역 조회
+     * @Parameter : [accessToken, accountId, pageable]
+     * @Return : Slice<ProductThumbResponse>
+     **/
     public Slice<ProductThumbResponse> findBuysByACcountId(String accessToken, Long accountId, Pageable pageable) {
         Account accountByAccessToken = accountService.findAccountByAccessToken(accessToken);
-        if(!accountByAccessToken.getAccountId().equals(accountId)) {
+        if (!accountByAccessToken.getAccountId().equals(accountId)) {
             throw new UnAuthorizedAccessException();
         }
         return tradeRepository.findBuyProductsByAccountId(accountId, pageable);
     }
 
+    /**
+     * @Method : findReviewsByAccountId
+     * @Description : 받은 리뷰 내역 조회
+     * @Parameter : [accountId, pageable]
+     * @Return : Slice<ReviewResponse>
+     **/
     public Slice<ReviewResponse> findReviewsByAccountId(Long accountId, Pageable pageable) {
         return reviewRepository.findReviewsByAccountId(accountId, pageable);
     }
 
+    /**
+     * @Method : findBoardsByAccountId
+     * @Description : 게시한 글 내역 조회
+     * @Parameter : [accountId, pageable]
+     * @Return : Slice<BoardSummaryResponse>
+     **/
     public Slice<BoardSummaryResponse> findBoardsByAccountId(Long accountId, Pageable pageable) {
         return boardRepository.findBoardsByAccountId(accountId, pageable);
     }
